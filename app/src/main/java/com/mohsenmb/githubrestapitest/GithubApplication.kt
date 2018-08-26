59c7aa3b567989995d7eb92092d92a1190cd7885
MohsenMB <mohsenmbcom@gmail.com>
2018-08-26 23:17:44 +0430

    Added application class to the app module

---
+++ app/src/main/java/com/mohsenmb/githubrestapitest/GithubApplication.kt
@@ -0,0 +1,9 @@
+package com.mohsenmb.githubrestapitest
+
+import android.app.Application
+
+class GithubApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
